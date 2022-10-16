404842722b1a4f6195faaec24a0da1d5dfaa52e9
Koenig-Heinrich-der-4te <84435773+Koenig-Heinrich-der-4te@users.noreply.github.com>
2022-10-16 22:41:44 +0200

    unicode-escaping string for irc transmisson

--- src/main/java/com/sonicboom/ircchat/client/util/ChatUtil.java
+++ src/main/java/com/sonicboom/ircchat/client/util/ChatUtil.java
@@ -3,6 +3,7 @@ package com.sonicboom.ircchat.client.util;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.*;
 import net.minecraft.util.Formatting;
+import org.apache.commons.text.StringEscapeUtils;
 
 public class ChatUtil {
     public static final MinecraftClient MC = MinecraftClient.getInstance();
@@ -12,11 +13,12 @@ public class ChatUtil {
     public static String getUsername(){
         return MC.player.getName().getString();
     }
-    public static void formatIrcMessage(String user,String message){
-        Text text=Text.literal("[")
+    public static void formatIrcMessage(String user, String message) {
+        String unescapedMessage = StringEscapeUtils.unescapeJava(message);
+        Text text = Text.literal("[")
                 .append(Text.literal(user).formatted(Formatting.GREEN))
                 .append(Text.literal("]"))
-                .append(Text.literal(" > "+message).formatted(Formatting.GREEN));
+                .append(Text.literal(" > " + unescapedMessage).formatted(Formatting.GREEN));
         MC.inGameHud.getChatHud().addMessage(text);
     }
 }
